77bc822e772a8f3c993814d1898e29b7f36bf13b
twoyoung91 <74153192+twoyoung91@users.noreply.github.com>
2023-01-09 12:11:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ import sys
 import pandas as pd
 import geopandas as gpd
 from ftplib import FTP
-from osgeo import gdal
 import zipfile
-
 
 abspath = os.path.abspath(sys.argv[0])
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
 import TigerReportFile2Shapefile as t2s
-import TigerReportFile2Shapefile_notgoodforAK as t2s_fast
+import TigerReportFile2Shapefile_fast as t2s_fast
 
 ftp=FTP('ftp2.census.gov')
 ftp.login()
@@ -28,6 +26,7 @@ ftp.cwd('geo/tiger/tiger2k')
 statelist=ftp.nlst()
 statelist=[x for x in statelist if len(x)==2]
 
+#download Tiger2k RT files and convert to shapefiles
 for state in statelist[28:]:
     if not os.path.exists(state):
         os.makedirs(state)
@@ -62,4 +61,34 @@ for state in statelist[28:]:
 
     ftp.cwd('..')
 
+#Merge shapefiles for each state
+if not os.path.exists("merged shapefiles"):
+    os.makedirs("merged shapefiles")
+for state in statelist:
+    if not os.path.exists("merged shapefiles/"+state+".shp"):
+        print ("merging shapefiles for "+state)
+        countylist=os.listdir(state)
+        countylist=[x for x in countylist if x[-3:]=="shp"]
+        stateshape=gpd.GeoDataFrame(columns=['id', 'x', 'y','type','geometry'], geometry='geometry',crs="epsg:4269")
+        for shapefile in countylist:
+            countyshape=gpd.read_file(state+"/"+shapefile)
+            stateshape=gpd.pd.concat([stateshape,countyshape])
+        if stateshape.shape[0]!=0:
+            stateshape.to_file("merged shapefiles/"+state+".shp")
+        else:
+            print (state+" does not have any street records.")
+    else:
+        print (state+" shapefile already exists, and it is skipped.")
+        
 
+#Merge state shapefiles into a national one
+nationshape=gpd.GeoDataFrame(columns=['id', 'x', 'y','type','geometry'], geometry='geometry',crs="epsg:4269")
+for state in statelist:
+    if not os.path.exists("merged shapefiles/"+state+".shp"):
+        print (state+" does not have a street shapefile, and it is skipped.")
+    else:
+        print ("merging shapefiles from "+state)
+        stateshape=gpd.read_file("merged shapefiles/"+state+".shp")
+        nationshape=gpd.pd.concat([nationshape,stateshape])
+nationshape.to_file("merged shapefiles/USstreets2000.shp")
+        
